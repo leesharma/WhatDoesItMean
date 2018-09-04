76bce967893526d55173f83fdfd1efe984327ad9
Lee Sharma <lee@leesharma.com>
2018-09-04 16:12:14 -0400

    Add checkstyle plugin
    I'm using the Google Java style since that's what's required for most
    UMUC classes.
    
    I've set this up to fail the build on warnings. This might be annoying,
    but I think it'll help us keep a consistent style throughout the
    project.
    
    Checkstyle can be run in two ways:
      1. `mvn validate` will check the style as a standalone
      2. `mvn package` will build the project, checking the style as a
         preliminary step. This is the command Travis will run.
    
    Locally, it might be easier to install a checkstyle plugin and run it
    through your IDE. IntelliJ has a nice one called CheckStyle-IDEA.
    
    Note: for some reason, `mvn checkstyle:check` uses the WRONG styleguide;
    don't use this for validation!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,14 +10,23 @@
 
 import javax.swing.JFrame;
 
+/**
+ * Entry point to the What Does It Mean? captioning application.
+ */
 public class Main {
-    public static void main (String [] args) {
-        JFrame frame = new JFrame("Hello World!");
 
-        frame.setSize(600, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+  /**
+   * Launches the application.
+   *
+   * @param args command-line arguments
+   */
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Hello World!");
 
-        System.out.println("Hello world! We've just launched a blank window.");
-    }
+    frame.setSize(600, 400);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+
+    System.out.println("Hello world! We've just launched a blank window.");
+  }
 }
