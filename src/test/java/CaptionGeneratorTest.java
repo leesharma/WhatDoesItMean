bd64104c01b39e9ad74197cb8f24f849cf0b3916
Lee Sharma <lee@leesharma.com>
2018-09-18 13:16:17 -0400

    Add test helper and determinism tests
    One of our criteria is that the same image outputs the same caption, so
    I added a couple tests to verify this. This'll prevent us from breaking
    this functionality as the captioning becomes more complex.
    
    In the process, I added some image fixtures and a test image loader for
    easy loading of test fixtures. I think this is cleaner than messing with
    `File.open` in the actual test cases, and it makes the test purpose
    clearer.

--- src/test/java/CaptionGeneratorTest.java
+++ src/test/java/CaptionGeneratorTest.java
@@ -11,23 +11,55 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CaptionGeneratorTest {
+  // provides convenience functions for accessing test images
+  private TestImageLoader fixtures = new TestImageLoader();
+
+  @BeforeEach
+  void resetImages() {
+    fixtures.reset(); // resets the image fixtures to a clean state
+  }
 
   @Test
   void generateCaption() {
     CaptionGenerator cg = new CaptionGenerator();
-    String caption  = cg.generateCaption(
-        new BufferedImage(400, 600, BufferedImage.TYPE_INT_RGB)
-    );
+    String caption  = cg.generateCaption(fixtures.getBlankImage());
 
     assertNotNull(caption); // verify that it runs
   }
 
   @Test
-  void generateCaption__nullImage() {
+  void generateCaption__failsWithNullInput() {
     CaptionGenerator cg = new CaptionGenerator();
     assertThrows(IllegalArgumentException.class, () -> cg.generateCaption(null));
+  }
+
+  @Test
+  void generateCaption__isDeterministic__sameObject() throws IOException {
+    CaptionGenerator cg = new CaptionGenerator();
+    BufferedImage img = fixtures.getNextImage();
+
+    // exact same object
+    assertEquals(
+        cg.generateCaption(img),
+        cg.generateCaption(img)
+    );
+  }
+
+  @Test
+  void generateCaption__isDeterministic__sameImage() throws IOException {
+    CaptionGenerator cg = new CaptionGenerator();
+    BufferedImage img = fixtures.getNextImage();
+    BufferedImage dupImg = fixtures.getCurrentImage();
+
+    // same image, different objects
+    assertEquals(
+        cg.generateCaption(img),
+        cg.generateCaption(dupImg)
+    );
   }
 }
